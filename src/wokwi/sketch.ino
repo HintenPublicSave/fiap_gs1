3fcf1aad88cb5e8797c7ff1b93f5b67853900e7f
Lesasouza <lesasouza@gmail.com>
2025-06-01 19:36:11 -0300

    Inclusão do sistema de alerta de enchentes do Wokwi

--- src/wokwi/sketch.ino
+++ src/wokwi/sketch.ino
@@ -1,36 +1,25 @@
-// Essa aplicação tem o objetivo verificar a necessidade de irrigação através da utilização de 4 sensores
-// Sensores: LDR (pH simulador), Umidade, Botão Azul (Fósforo Simulado) e Botão Amarelo (Potássio Simulado)
+// Essa aplicação tem o objetivo enviar alertas de enchentes através da utilização de 2 sensores
+// Sensores: LDR (Nivel simulado), 
 // Quando 2 ou mais sensores apontarem resultados negativos o relé do sistema de irrigação será acionado e o led irá acender
 // Cado API Meteorológica. representada pelo Botão Vermelho, informe que que haverá chuva o sistema de irrigação será interrompido
 
 // CONDIÇÃO NEGATIVA DE CADA SENSOR:
-// LDR (pH simulador): Valor > 7 (Aplicado um fator de divisão por 100, por pH só vai de 0 a 14 e o LDR vai de 0 a 4000), 
-// Umidade: Valor < 60% 
-// Botão Azul (Fósforo Simulado): Desligado
-// Botão Amarelo (Potássio Simulado): Desligado
+// LDR (Nivel simulador): Valor > 3000 (Aplicado um fator de divisão por 10, altura de agua em cm, vai de 0 a 400 e o LDR vai de 0 a 4000), 
 
-#include <DHT.h> // Inclusão da biblioteca do sensor DHT22
 
 // === DEFINIÇÃO DE PINOS ===
-#define BUTTON_P 5        // Botão de fósforo (azul)
-#define BUTTON_K 4        // Botão de potássio (amarelo)
-#define LDR_PIN 14        // Pino analógico para simular pH via LDR
-#define DHTPIN 12         // Sensor DHT22 (umidade)
-#define DHTTYPE DHT22     // Tipo do sensor
+#define LDR_PIN 14        // Pino analógico para simular nivel de agua em bueiros via LDR
 #define RELAY_PIN 34      // Relé que aciona a bomba
 #define LED_PIN 2         // LED indicativo da bomba
 #define BUTTON_API 18     // Botão de API Meteorológica (vermelho)
+#define ECHO_PIN 25       // ECHO (Medição do comprimento de um pulso alto para obter a distância) HC-SR04 nível de água
+#define TRIG_PIN 26       // TRIG (Pulso para iniciar a medição) HC-SR04 Nível de água
+#define buzzer 23         // Buzzer de alerta
 
-// === OBJETO DO SENSOR DHT ===
-DHT dht(DHTPIN, DHTTYPE);
+
+// === OBJETO DO SENSOR HC-SR04 ===
 
 // === VARIÁVEIS DE ESTADO DOS BOTÕES ===
-bool estadoFosforo = false;
-bool ultimoEstadoFosforo = HIGH;
-
-bool estadoPotassio = false;
-bool ultimoEstadoPotassio = HIGH;
-
 bool estadoAPI = false;
 bool ultimoEstadoAPI = HIGH;
 
@@ -38,39 +27,36 @@ void setup() {
   // Inicializa comunicação serial
   Serial.begin(115200);
 
+  // Configuração da entrada e saída do sensor de nível
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
   // Configuração dos pinos de entrada
-  pinMode(BUTTON_P, INPUT_PULLUP);  // Botão de fósforo
-  pinMode(BUTTON_K, INPUT_PULLUP);  // Botão de potássio
   pinMode(BUTTON_API, INPUT_PULLUP);  // Botão da API
 
   // Configuração dos pinos de saída
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 
-  // Inicializa o sensor DHT
-  dht.begin();
+}
+
+// Parâmetros do sensor de nível de água
+float readDistanceCM() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  int duration = pulseIn(ECHO_PIN, HIGH);
+  return duration * 0.034 / 2;
 }
 
 void loop() {
   // === LEITURA E CONTROLE DE ESTADO DOS BOTÕES ===
-  bool leituraFosforo = digitalRead(BUTTON_P);
-  bool leituraPotassio = digitalRead(BUTTON_K);
   bool leituraAPI = digitalRead(BUTTON_API);
   bool LedValue = digitalRead(LED_PIN);
-
-  // Verifica mudança de estado do botão de fósforo
-  if (leituraFosforo == LOW && ultimoEstadoFosforo == HIGH) {
-    estadoFosforo = !estadoFosforo; // Alterna o estado
-    delay(200); // Debounce
-  }
-  ultimoEstadoFosforo = leituraFosforo;
-
-  // Verifica mudança de estado do botão de potássio
-  if (leituraPotassio == LOW && ultimoEstadoPotassio == HIGH) {
-    estadoPotassio = !estadoPotassio; // Alterna o estado
-    delay(200); // Debounce
-  }
-  ultimoEstadoPotassio = leituraPotassio;
+  bool BuzValue = digitalRead(buzzer);
 
   // Verifica mudança de estado do botão da API
   if (leituraAPI == LOW && ultimoEstadoAPI == HIGH) {
@@ -79,32 +65,29 @@ void loop() {
   }
   ultimoEstadoAPI = leituraAPI;
 
-  // Leitura do LDR (simulando pH)
+  // Leitura do LDR (simulando Nível de Agua no Bueiro)
   int ldrValue = analogRead(LDR_PIN);  // Faixa de 0 a 4095 no ESP32
 
-  // Leitura da umidade com o DHT22
-  float umidade = dht.readHumidity();
 
   // === CONDIÇÕES DE IRRIGAÇÃO ===
 
-  // Verifica se fósforo e potássio estão presentes
-  bool nutrientesPresentes = !estadoFosforo && !estadoPotassio;
 
- // Verifica se fósforo e potássio estão presentes
+ // Verifica o sinal da API foi recebido
   bool resultadoAPI = !estadoAPI;
 
-  // Condição de pH ideal (simulado pelo LDR)
-  bool phIdeal = (ldrValue > 700);
+  // Nivel de água no bueiro ideal (simulado pelo LDR)
+  bool nivelIdeal = (ldrValue > 3000);
 
-  // Verifica se a umidade está abaixo do ideal (< 60%)
-  bool umidadeBaixa = umidade < 60;
+  // Verifica se a aguá está no nível aceitável (> 3m)
+  float distance = readDistanceCM();
+
+  bool isNearby = distance > 300;
+  //digitalWrite(LED_BUILTIN, isNearby);
 
   // Mostrar valores no Serial Monitor
-  Serial.print("Fósforo: "); Serial.print(estadoFosforo);
-  Serial.print(" | Potássio: "); Serial.print(estadoPotassio);
-  Serial.print(" | LDR (pH simulado): "); Serial.print(ldrValue / 100.0);
-  Serial.print(" | Umidade: "); Serial.print(umidade); Serial.print("%");
-  Serial.print(" | relé (Irrigacao): "); Serial.print(LedValue);
+  Serial.print("LDR (Nível Bueiro): "); Serial.print(ldrValue / 10.0); Serial.print("cm");
+  Serial.print(" | Nível Leito: "); Serial.print(distance); Serial.print("cm");
+  Serial.print(" | Relé (Envio de Dados): "); Serial.print(LedValue);
   Serial.print(" | API: "); Serial.println(estadoAPI);
    
   // === AÇÃO: ATIVAR BOMBA ===
@@ -114,20 +97,31 @@ void loop() {
   if (estadoAPI) condicoesAPI++;
   // Conta quantas variáveis estão com valor falso (condição crítica)
   int condicoesCriticas = 0;
-  if (!estadoFosforo) condicoesCriticas++;
-  if (!estadoPotassio) condicoesCriticas++;
-  if (phIdeal) condicoesCriticas++;
-  if (umidadeBaixa) condicoesCriticas++;
+  if (nivelIdeal) condicoesCriticas++;
+  if (isNearby) condicoesCriticas++;
 
-  // Caso 2 ou mais sensores acusem problema a irrigação será acionado
-  // Caso a API comunique que haverá chuva a irrigação será desativada
-  if (condicoesCriticas >= 2 && condicoesAPI == 0) {
+  // Caso pelo menos 1 dos sensores aponte nível elevado de água e a API acuse previsão de chuva o alerta será emitido
+  if (condicoesCriticas >= 1 && condicoesAPI == 1) {
     digitalWrite(RELAY_PIN, HIGH);  // Liga a bomba
     digitalWrite(LED_PIN, HIGH);    // Liga o LED indicativo
+
+    // Toca a sequência de tons
+    tone(buzzer, 261);
+    delay(100);
+    tone(buzzer, 293);
+    delay(100);
+    tone(buzzer, 329);
+    delay(100);
+    tone(buzzer, 349);
+    delay(100);
+    tone(buzzer, 392);
+    delay(100);
+    noTone(buzzer);
   } else {
     digitalWrite(RELAY_PIN, LOW);   // Desliga a bomba
     digitalWrite(LED_PIN, LOW);     // Desliga o LED
+    noTone(buzzer);
   }
 
-  delay(1000);  // Espera 1 segundo antes da próxima leitura
+  delay(2000);  // Espera 1 segundo antes da próxima leitura
 }
