bc0120e13a955cc908a1a9b654ce80d6aebe2df3
luxyvsc <pedro.tostessilva1@gmail.com>
2025-06-02 20:13:37 -0300

    atualizando git_ignore

--- src/large_language_model/tools/previsao_de_enchentes.py
+++ src/large_language_model/tools/previsao_de_enchentes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import os
 import io
 import contextlib
-from src.API_met import get_5day_forecast, analisar_chuva_5day
+from src.API_met.api import get_5day_forecast, analisar_chuva_5day
 from dotenv import load_dotenv
 from src.large_language_model.tipos_base.base_tools import BaseTool
 
